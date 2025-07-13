a4f045636c7a227ad0b811a478152c924fd658c2
BAHAUDDIN CHISHTE <116437546+bahauddinchishte2@users.noreply.github.com>
2025-07-13 10:21:59 +0600

    Updated clients.ts

---
+++ src/data/clients.ts
@@ -0,0 +1,14 @@
+export const majorClients = [
+  { name: 'Nestlé', logo: '/images/clients-logo/Nestle-Logo.jpg' },
+  { name: 'Coca-Cola', logo: '/images/clients-logo/Coca-Cola-Logo.jpg' },
+  { name: 'US Bangla Airlines', logo: '/images/clients-logo/Biman-Bangladesh-Logo.jpg' },
+  { name: 'Pizza Hut', logo: '/images/clients-logo/Pizza-Hut-Logo.jpg' },
+  { name: 'Apex', logo: '/images/clients-logo/Apex-logo.jpg' },
+  { name: 'Starbucks', logo: '/images/clients-logo/Starbucks-Logo.jpg' },
+  { name: 'Cadet College Club', logo: '/images/clients-logo/Cadet-College-Club-Logo.jpg' },
+  { name: 'KFC', logo: '/images/clients-logo/KFC-Logo.jpg' },
+  { name: 'Bata', logo: '/images/clients-logo/Bata-Logo.jpg' },
+  { name: 'Dhaka Club', logo: '/images/clients-logo/Dhaka-Club-Logo.jpg' },
+  { name: 'Gulshan Club', logo: '/images/clients-logo/Gulshan-Club-Logo.jpg' },
+  { name: 'Us-Bangla', logo: '/images/clients-logo/Us-Bangla-logo.jpg' },
+];
